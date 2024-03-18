8e44170026542d20c568a8d0ce819ebe15efa480
sourabh696969 <sourabhkumar2462@gmail.com>
2024-03-18 14:48:42 +0530

    update

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ["http://localhost:5173"];
-app.use(
-  cors({
-    origin: allowedOrigins,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api", require("./Routes/routes"));
 app.use("/images", express.static("upload/images"));
